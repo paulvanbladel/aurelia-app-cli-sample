a6323ffb24f0f3a96fedec37ea07105fe26e65ad
paul van bladel <paul.vanbladel@gmail.com>
2016-09-15 16:20:18 +0200

    change router default path to cli-intro

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: 'cli-introitus',         name: 'cli-introitus',        moduleId: './cli-introitus',        nav: true, title: 'cli-introitus' },
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title: 'Welcome' },
+      { route: ['','cli-introitus'],         name: 'cli-introitus',        moduleId: './cli-introitus',        nav: true, title: 'cli-introitus' },
+      { route: 'welcome', name: 'welcome',      moduleId: './welcome',      nav: true, title: 'Welcome' },
       { route: 'child-router',  name: 'child-router', moduleId: './child-router', nav: true, title: 'Child Router' }
     ]);
 
