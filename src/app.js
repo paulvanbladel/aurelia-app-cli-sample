72f20f9fcc2e485e3fa2135558383de694073647
paul van bladel <paul.vanbladel@gmail.com>
2016-09-15 16:22:56 +0200

    renamed intro page

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['','cli-introitus'],         name: 'cli-introitus',        moduleId: './cli-introitus',        nav: true, title: 'cli-introitus' },
+      { route: ['','cli-intro'],         name: 'cli-intro',        moduleId: './cli-intro',        nav: true, title: 'cli-intro' },
       { route: 'welcome', name: 'welcome',      moduleId: './welcome',      nav: true, title: 'Welcome' },
       { route: 'child-router',  name: 'child-router', moduleId: './child-router', nav: true, title: 'Child Router' }
     ]);
